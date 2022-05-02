b3242059974d3aa6ecd235d18b5dac3605fb8f3e
Anno <80528457+AnnoDSC@users.noreply.github.com>
2022-05-02 19:58:49 +0200

    Full Auto

--- nc.sh
+++ nc.sh
@@ -6,7 +6,23 @@ unzip nextcloud-20.0.1.zip
 mv nextcloud /var/www
 rm nextcloud-20.0.1.zip
 
-mv nextcloud.conf /etc/apache2/sites-available
+echo -e '
+Alias /nextcloud "/var/www/nextcloud/"
+
+<Directory /var/www/nextcloud/>
+  Options +FollowSymlinks
+  AllowOverride All
+
+ <IfModule mod_dav.c>
+  Dav off
+ </IfModule>
+
+ SetEnv HOME /var/www/nextcloud
+ SetEnv HTTP_HOME /var/www/nextcloud
+
+</Directory>
+' >> /etc/apache2/sites-available/nextcloud.conf
+
 sudo a2ensite nextcloud.conf
 sudo a2enmod rewrite headers env dir mime setenvif ssl
 
